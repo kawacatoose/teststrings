5ec981ea82920c42c19422866b807f06a7554bcb
kawacatoose <heerovanderveen@outlook.com>
2025-03-20 13:59:25 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # teststrings
+
+Task
+
+Write a script which reads two strings as arguments and then:
+
+    1. Tests to see if the first string is of zero length, and if the other is of non-zero length, telling the user of both results.
+    2. Determines the length of each string, and reports on which one is longer or if they are of equal length.
+    3. Compares the strings to see if they are the same, and reports on the result.
+
---
+++ teststrings.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ARGUM=$#
+STR1=$1
+STR2=$2
+STR1LEN=${#STR1}
+STR2LEN=${#STR2}
+
+if [[ -z "$STR1" ]]; then
+	echo First string is empty.
+	echo
+fi
+if [[ -n "$STR1" ]]; then
+	echo First string is not empty.
+	echo
+fi 
+if [[ -z "$STR2" ]]; then
+	echo Second string is empty.
+	echo
+fi
+if [[ -n $STR2 ]]; then
+	echo Second string is not empty.
+	echo
+fi
+
+if [[ $ARGUM -eq 2 ]]; then
+	if [[ $STR1LEN > $STR2LEN ]]; then
+		echo The first string is longer.
+		echo
+	fi
+	if [[ $STR1LEN < $STR2LEN ]]; then
+		echo The second string is longer.
+		echo
+	fi
+	if [[ $STR1LEN = $STR2LEN ]]; then
+		echo The first and second string are equal length.
+		echo
+	fi
+	if [[ $STR1 == $STR2 ]]; then
+		echo The first string is the same as the second string.
+		echo
+	else
+		echo The first string is not the same as the second string.
+	fi
+else
+	echo Need 2 strings.
+	echo
+fi
